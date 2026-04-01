a43ce7f6df31aaf0b08d0d460bd4db1431874647
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-04-01 11:07:07 -0700

    Todo list working

--- Desktop/react-lab/src/App.jsx
+++ Desktop/react-lab/src/App.jsx
@@ -1,9 +1,25 @@
+import './App.css'
+
 function App() {
+  const todoList = [
+        {id: 1, title: "review resourse"},
+        {id: 2, title: "take notes"},
+        {id: 3, title: "code out app"}
+
+      ]
+      
   return (
     <div>
-      <h1>Hello Nasistu </h1>
-      <p>I built my first React app!</p>
+      <h1>My Todos </h1>
+
+    <ul>
+      {todoList.map(todo =>(
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+      
     </div>
+
   );
 }
 export default App;
