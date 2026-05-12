85001992c4fb5b86d53f1d8076b9e62e07c2daf1
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-05-11 23:32:06 -0700

    Create resusable TextInputWithLable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
 import './App.css';
 import { useState } from 'react';
-import TodoList from './TodoList';
-import TodoForm from './TodoForm';
-// Lesson 02 components setup 
+import TodoForm from './features/TodoForm';
+import TodoList from './features/TodoList/TodoList';
 
 
-function App() {   
+function App() {
   const [todoList, setTodoList] = useState([]);
 
   const addTodo = (todoTitle) => {
-    const newTodo = {  
+    const newTodo = {
       id: Date.now(),
       title: todoTitle,
+      isCompleted: false,
     };
+
     setTodoList((prev) => [...prev, newTodo]);
   };
+
+  function completeTodo(id) {
+    const updatedTodoList = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+
+      return todo;
+    });
+
+    setTodoList(updatedTodoList);
+  }
+
   return (
     <div className="app-container">
       <h1>My Todos List</h1>
-<TodoForm onAddTodo={addTodo}/>
- <TodoList todoList={todoList} />
-    </div>
-  )
 
-};
+      <TodoForm onAddTodo={addTodo} />
+
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+      />
+    </div>
+  );
+}
 
 export default App;
+// Lesson 02 components setup 
+
+
