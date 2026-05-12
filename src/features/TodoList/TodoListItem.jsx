751c3d3e0885afc2537bc37c3183d3ba476dcd57
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-05-12 00:06:11 -0700

    Add editable todo state and reusable input component

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,4 +1,65 @@
-function TodoListItem({todo}){ 
-    return <li>{todo.title}</li>
-} 
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+
+function TodoListItem({
+  todo,
+  onCompleteTodo,
+}) {
+  const [isEditing, setIsEditing] =
+    useState(false);
+
+  const [workingTitle, setWorkingTitle] =
+    useState(todo.title);
+
+  return (
+    <li>
+      <form>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel
+              value={workingTitle}
+              elementId={`todo-${todo.id}`}
+              labelText="Edit Todo"
+              onChange={(event) =>
+                setWorkingTitle(event.target.value)
+              }
+            />
+
+            <button
+              type="button"
+              onClick={() => {
+                setWorkingTitle(todo.title);
+                setIsEditing(false);
+              }}
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                id={`checkbox${todo.id}`}
+                checked={todo.isCompleted}
+                onChange={() =>
+                  onCompleteTodo(todo.id)
+                }
+              />
+            </label>
+
+            <span
+              onClick={() =>
+                setIsEditing(true)
+              }
+            >
+              {todo.title}
+            </span>
+          </>
+        )}
+      </form>
+    </li>
+  );
+}
+
 export default TodoListItem;
