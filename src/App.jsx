7320ba5ea07a350a7352525fcb023c98120f9502
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-05-17 12:26:06 -0700

    Set up Header and TodosPage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,11 @@
-import './App.css';
-import { useState } from 'react';
-import TodoForm from './features/TodoForm';
-import TodoList from './features/TodoList/TodoList';
-
-function App() {
-  const [todoList, setTodoList] = useState([]);
-
-  const addTodo = (todoTitle) => {
-    const newTodo = {
-      id: Date.now(),
-      title: todoTitle,
-      isCompleted: false,
-    };
-
-    setTodoList((prev) => [...prev, newTodo]);
-  };
-
-  function completeTodo(id) {
-    const updatedTodoList = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: true };
-      }
-
-      return todo;
-    });
-
-    setTodoList(updatedTodoList);
-  }
-
-  const updateTodo = (updatedTodo) => {
-    const updatedTodoList = todoList.map((todo) =>
-      todo.id === updatedTodo.id ? updatedTodo : todo
-    );
-
-    setTodoList(updatedTodoList);
-  };
-
-  return (
-    <div className="app-container">
-      <h1>My Todos List</h1>
-
-      <TodoForm onAddTodo={addTodo} />
-
-      <TodoList
-        todoList={todoList}
-        onCompleteTodo={completeTodo}
-        onUpdateTodo={updateTodo}
-      />
-    </div>
+import Header from "./shared/Header";
+import TodosPage from "./features/Todos/TodosPage";
+function App(){ 
+  return(
+    <>
+    <Header/>
+    <TodosPage/>
+    </>
   );
 }
-
 export default App;
---
+++ src/features/Todos/TodosPage.js
@@ -0,0 +1,8 @@
+function TodosPage(){
+    return(
+        <div>
+            <h2>Todos Page</h2>
+        </div>
+    );
+}
+export default TodosPage;
