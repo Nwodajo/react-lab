65ad7b350d4b26dbb3c3eef2fb2c4a53f8c5f049
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-05-30 22:51:09 -0700

    Create AuthContext and wrap app with provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>
+</StrictMode>
 )
