8e1cfe03143253905455dccf6cd284b3144815ee
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-06-08 23:24:48 -0700

    feat: add navigation to header

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from '../contexts/AuthContext';
+import Navigation from './Navigation';
 
 function Header() {
   const { token, logout } = useAuth();
@@ -6,6 +7,8 @@ function Header() {
   return (
     <header>
       <h1>Todo List</h1>
+
+      <Navigation />
 
       {token && (
         <button onClick={logout}>
