20ba0864418aaddc8c29d5c057f15d10c76c27c4
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-06-08 20:55:57 -0700

    Add home link to NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,21 @@
-function NotFoundPage() {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+import { Link } from 'react-router-dom';
 
-      <p>
-        Sorry, the page you are looking for does not exist.
-      </p>
-    </div>
-  );
+function NotFoundPage() {
+return (
+<div>
+<h1>404 - Page Not Found</h1>
+
+<p>
+Sorry, the page you are looking for does not exist.
+</p>
+
+<p>
+Use the link below to return to the home page.
+</p>
+
+<Link to="/">Go Home</Link>
+</div>
+);
 }
 
 export default NotFoundPage;
