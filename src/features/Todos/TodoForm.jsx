c9c91b742f7a5634f377db3d67625cca1e4f39b1
Nwodajo <120424900+Nwodajo@users.noreply.github.com>
2026-06-19 12:32:39 -0700

    Add todo input length limit and error message

--- src/features/Todos/TodoForm.jsx
+++ src/features/Todos/TodoForm.jsx
@@ -5,22 +5,22 @@ import { sanitizeInput } from '../../utils/sanitizeInput';
 
 function TodoForm({ onAddTodo }) {
   const inputRef = useRef();
-
   const [workingTodoTitle, setWorkingTodoTitle] = useState('');
+  const maxTodoLength = 100;
 
   const handleAddTodo = (event) => {
     event.preventDefault();
 
     const todoTitle = sanitizeInput(workingTodoTitle);
 
-    if (todoTitle) {
+    if (todoTitle && todoTitle.length <= maxTodoLength) {
       onAddTodo(todoTitle);
-
       setWorkingTodoTitle('');
-
       inputRef.current.focus();
     }
   };
+
+  const isTooLong = workingTodoTitle.length > maxTodoLength;
 
   return (
     <form onSubmit={handleAddTodo}>
@@ -29,14 +29,23 @@ function TodoForm({ onAddTodo }) {
         labelText="Todo"
         ref={inputRef}
         value={workingTodoTitle}
+        maxLength={100}
         onChange={(event) =>
           setWorkingTodoTitle(event.target.value)
         }
       />
 
+      {isTooLong && (
+        <p className="error-message">
+          Todo must be 100 characters or less.
+        </p>
+      )}
+
       <button
         type="submit"
-        disabled={!isValidTodoTitle(workingTodoTitle)}
+        disabled={
+          !isValidTodoTitle(workingTodoTitle) || isTooLong
+        }
       >
         Add Todo
       </button>
